7d5c5c24af5239fdf375861450a65d8d2936ae81
Joseph Zica <josephalmedazica@gmail.com>
2023-05-02 13:57:19 -0300

    adding prompt call

--- main.py
+++ main.py
@@ -1,21 +1,52 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
+import requests
 
-st.write("Here's our first attempt at using data to create a table:")
-st.write(pd.DataFrame({
-    'first column': [1, 2, 3, 4],
-    'second column': [10, 20, 30, 40]
-}))
+def execPrompt(prompt,key):
+    url = "https://secure-backend-api.stilingue.com.br/blip-nlu-hack-ai/prod/hack-ai/completions"
 
-st.text_input("Your name", key="name")
+    headers = {
+        "Content-Type": "application/json",
+        "hack-ai-team-name": "askblip",
+        "hack-ai-api-key": key,
+        "accept": "application/json"
+    }
 
-# You can access the value at any point with:
-st.session_state.name
+    data = {
+    "deployment": "text-davinci-003",
+    "prompt": prompt,
+    "max_tokens": 500,
+    "temperature": 0.7,
+    "top_p": 1,
+    "logit_bias": {},
+    "user": "TimeHackAi",
+    "n": 1,
+    "logprobs": 0,
+    "suffix": "",
+    "echo": False,
+    "stop": "string",
+    "presence_penalty": 0,
+    "frequency_penalty": 0,
+    "best_of": 1
+    }
 
-if st.checkbox('Show dataframe'):
-    chart_data = pd.DataFrame(
-       np.random.randn(20, 3),
-       columns=['a', 'b', 'c'])
+    response = requests.post(url, headers=headers, json=data)
 
-    chart_data
+    return response.json()['choices'][0]['text']
+
+st.title('AskBlip')
+
+key = st.text_input('Api Key:')
+
+txt = st.text_area('Resposta:',placeholder='Insira aqui o conteúdo da resposta')
+
+
+if st.button('Avaliar'):
+    r = execPrompt('prompt avaliacao' + txt,key)
+    st.text_area('Avaliação:',value=txt, disabled=True)
+
+if st.button('Reescrever'):
+    r = execPrompt('prompt reescrita' + txt,key)
+    st.text_area('Reescrita:',value=r, disabled=True)
+
+
+
